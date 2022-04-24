1a414abf07b3ac32959ad216e8c415a6291443f4
unknown <michaelwesleyh@gmail.com>
2022-04-23 20:08:04 -0500

    Adds new line

--- SimplePasswordApp.py
+++ SimplePasswordApp.py
@@ -119,3 +119,4 @@ while True:
         authenticationSet(change)
     else:
         userInput = str(input("Please input either 'lookup', 'create' or 'create password', 'change', or 'quit'"))  # failsafe response
+
